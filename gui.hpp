59811ac52392b64c00fb46807612ce5800177247
Samia Rahman Prapti <166444266+srprapti@users.noreply.github.com>
2025-08-14 11:03:29 +0600

    gui.hpp

--- gui.hpp
+++ gui.hpp
@@ -1,13 +1,1 @@
-#ifndef GUI_HPP
-#define GUI_HPP
 
-#include <vector>
-#include <string>
-
-void launchGUI(const std::vector<char>& sequence,
-               const std::vector<std::vector<std::pair<int, int>>>& graph,
-               const std::vector<int>& shortestPath,
-               const std::vector<std::string>& properties,
-               const std::vector<std::pair<int, int>>& misfoldEdges);
-
-#endif
